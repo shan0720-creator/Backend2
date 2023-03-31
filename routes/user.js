e8c3c1806c1c3d0a1338e757c948f3ce6982ebd9
shantanu sahay <71556064+shan0720-creator@users.noreply.github.com>
2023-04-01 03:06:54 +0530

    Initial Commit 2

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {  getMyProfile, login, register,logout } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/Auth.js";
-import {User} from "../models/user.js"
+//import {User} from "../models/user.js"
 
 const router = express.Router();
 
